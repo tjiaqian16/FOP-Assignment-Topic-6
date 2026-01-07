ee8a377532ce2eebff0e86b3db94c00fb5ff46d8
Priscilia Cheong Ee Cheng <23121100@siswa.um.edu.my>
2026-01-07 14:59:25 +0800

    Create MainInterface.java

---
+++ MainInterface.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class MainInterface extends JFrame {
+    // We need these methods to exist so the other pages can call them without error.
+    
+    public void showView(String viewName) {
+        // Placeholder: Will switch screens later
+    }
+
+    public void startGame(String playerName, int level) {
+        // Placeholder: Will start the game later
+    }
+}
