63d5f9ae9a0c3213561384a72d04db8e948eea66
Jia Qian <tanjiaqian0296@gmail.com>
2026-01-10 00:06:34 +0800

    latest

--- GameLoader.java
+++ GameLoader.java
@@ -8,26 +8,26 @@ public class GameLoader {
 
     public GameLoader(String filename) throws Exception {
         Scanner sc = new Scanner(new File(filename));
-        this.targetPiece = sc.nextInt(); // Line 1: Target [cite: 93]
+        this.targetPiece = sc.nextInt(); // Line 1: Target 
         for (int i = 0; i < 6; i++) {
-            this.initialPositions[i] = sc.nextInt(); // Line 2: Initial Pos [cite: 94]
+            this.initialPositions[i] = sc.nextInt(); // Line 2: Initial Pos 
         }
         while (sc.hasNextInt()) {
-            this.diceSequence.add(sc.nextInt()); // Line 3: Dice Sequence [cite: 95]
+            this.diceSequence.add(sc.nextInt()); // Line 3: Dice Sequence 
         }
         sc.close();
     }
 
     public void printGameDetails(String playerName, PrintWriter out) {
-        out.println(playerName); // [cite: 98]
+        out.println(playerName); 
         for (int i = 0; i < diceSequence.size(); i++) {
             out.print(diceSequence.get(i) + (i == diceSequence.size() - 1 ? "" : " "));
         }
-        out.println(); // [cite: 99]
-        out.println(targetPiece); // [cite: 100]
+        out.println(); 
+        out.println(targetPiece); 
         for (int i = 0; i < 6; i++) {
             out.print(initialPositions[i] + (i == 5 ? "" : " "));
         }
-        out.println(); // [cite: 101]
+        out.println(); 
     }
 }
